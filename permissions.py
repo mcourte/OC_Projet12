be04e59c454988a192891249de64f669909ea145
Magali Courté <courte.magali@gmail.com>
2024-07-30 16:53:17 +0200

    ajout fichier permissions

---
+++ permissions.py
@@ -0,0 +1,18 @@
+#  from models.customer import Customer
+#  from models.user import User
+
+
+class IsAuthenticad:
+    pass
+
+
+class UserGes:
+    pass
+
+
+class UserCom:
+    pass
+
+
+class UserSup:
+    pass
