aa0ad72f71dc92b520d0c66c4110d5c96e8ed63b
Magali Courté <courte.magali@gmail.com>
2024-07-31 14:57:33 +0200

    ajout pythonpath

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
 from dotenv import load_dotenv
 import os
+import sys
 
+# Charger les variables d'environnement depuis le fichier .env
 load_dotenv()
 
+# Ajouter le répertoire racine du projet au PYTHONPATH
+project_root = os.getenv('PYTHONPATH')
+if project_root:
+    sys.path.append(project_root)
+else:
+    print("PYTHONPATH n'est pas défini dans .env")
+
+# Configurations de la base de données
 db_user = os.getenv('DB_USER', 'app_user')
 db_password = os.getenv('DB_PASSWORD', 'password')
 db_host = os.getenv('DB_HOST', 'localhost')
