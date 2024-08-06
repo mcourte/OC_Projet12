48ecfd7e89dac807129736d7d89c4e613c171982
Magali Courté <courte.magali@gmail.com>
2024-08-06 08:55:26 +0200

    màj des constantes

--- constantes.py
+++ constantes.py
@@ -22,3 +22,30 @@ SORT_USER = "sort_user"
 SORT_CUSTOMER = "sort_user"
 SORT_CONTRACT = "sort_user"
 SORT_EVENT = "sort_user"
+
+
+# Constantes pour le tri des élements avec la méthode générique
+def get_sortable_attributes():
+    from models.contract import Contract
+    from models.user import User
+    from models.event import Event
+    from models.customer import Customer
+    from permissions import Role
+
+    return {
+        User: {
+            Role.ADM: ['user_id', 'first_name', 'last_name', 'username'],
+        },
+        Contract: {
+            Role.ADM: ['contract_id', 'ges_contact_id', 'com_contact_id', 'remaining_amount', 'is_signed'],
+            Role.COM: ['is_signed', 'remaining_amount']
+        },
+        Customer: {
+            Role.ADM: ['last_name', 'company_name', 'email', 'com_contact_id', 'creation_time', 'update_time'],
+        },
+        Event: {
+            Role.ADM: ['event_id', 'ges_contact_id', 'ges_support_id', 'customer_id', 'event_date', 'location'],
+            Role.GES: ['sup_contact_id'],
+            Role.SUP: ['sup_contact_id']
+        }
+    }
