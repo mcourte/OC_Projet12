3f91297855e7b5bd59b4e6cecd9a622a000b9148
Magali Courté <courte.magali@gmail.com>
2024-08-12 21:54:08 +0200

    màj .gitignore

--- test.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from config import Base
-
-engine = create_engine('sqlite:///:memory:')
-Base.metadata.drop_all(engine)  # Supprimer toutes les tables
-Base.metadata.create_all(engine)  # Recréer toutes les tables
