ce14ea05f9c9e68bba22a9524fc05d4aac8a9413
Magali Courté <courte.magali@gmail.com>
2024-08-16 08:29:18 +0200

    suppression setup_db

--- setup_db.py
+++
@@ -1,7 +0,0 @@
-# setup_db.py
-from sqlalchemy import create_engine
-from config import Base
-
-# Création de l'engin SQLite pour les tests
-engine = create_engine('sqlite:///test.db')
-Base.metadata.create_all(engine)  # Crée toutes les tables définies dans Base
