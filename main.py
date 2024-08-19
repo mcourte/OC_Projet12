5b5cdcbfb6f9df68abb3ff447129f63167389281
Magali Courté <courte.magali@gmail.com>
2024-08-19 18:32:47 +0200

    test main

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from database import SessionLocal
-from controllers import UserController
+import click
+from cli import contract_cli, customer_cli, epic_cli, event_cli, user_cli
 
 
+@click.group(help="------ CRM EpicEvent ------")
 def main():
-    session = SessionLocal()
-    user_controller = UserController(session)
+    pass
 
 
-if __name__ == "__main__":
+main.add_command(epic_cli.login)
+main.add_command(epic_cli.logout)
+main.add_command(epic_cli.dashboard)
+main.add_command(epic_cli.initbase)
+main.add_command(user_cli)
+main.add_command(customer_cli)
+main.add_command(contract_cli)
+main.add_command(event_cli)
+
+
+if __name__ == '__main__':
     main()
